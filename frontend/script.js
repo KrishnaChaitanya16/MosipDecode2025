40f1c48bf6bfca5b5fd011cd30e11302eab9c9b7
KrishnaChaitanya16 <16krish2005@gmail.com>
2025-09-08 19:02:57 +0530

    Modified hosturi

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,5 @@
 (() => {
-  const apiBase = localStorage.getItem('apiBase') || 'http://localhost:8000';
+  const apiBase = localStorage.getItem('apiBase') || 'http://127.0.0.1:5000';
 
   const uploadForm = document.getElementById('upload-form');
   const documentInput = document.getElementById('document');
